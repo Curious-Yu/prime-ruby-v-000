adb99189a918f1c5a91b8eb5b2c3453d489d7531
Wen Yu <wyu10amy@hotmail.com>
2021-06-07 04:31:53 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,11 +3,6 @@ def prime?(number)
   prime_number = true
   if number < 2
     prime_number = false
-  else
-    for n in 2..number-1
-      if number % n == 0
-        prime_number = false
-      end
-    end
+
   end
 end
